9be51bbe6ade225f1a05969a11c6f3ad1c81402d
Saksham Gupta <www.rameshgupta67@gmail.com>
2023-08-27 13:22:41 +0530

    tuned the parameters a bit. Model still overfits

--- model.py
+++ model.py
@@ -9,10 +9,10 @@ from sklearn.preprocessing import LabelEncoder
 from torch.nn.utils.rnn import pad_sequence
 
 class SentimentAnalysisModel(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, num_layers):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, num_layers, dropout_prob):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=num_layers, batch_first=True)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=num_layers, batch_first=True, dropout=dropout_prob)
         self.relu = nn.ReLU()
         self.fc = nn.Linear(hidden_dim, output_dim)
 
@@ -31,22 +31,23 @@ embedding_dim = 32
 hidden_dim = 100
 num_classes = 4
 output_dim = num_classes
-num_layers = 2
+num_layers = 4
 num_epochs = 100
 batch_size = 32
+dropout_prob = 0.6
 
 # Initialize the model
-model = SentimentAnalysisModel(vocab_size=vocab_size, embedding_dim=embedding_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
+model = SentimentAnalysisModel(vocab_size=vocab_size, embedding_dim=embedding_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers, dropout_prob=dropout_prob)
 print(model)
 
 # Define loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.005)
+optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-5)
 
 # Load dataset
 path = 'Dataset/dataset.csv'
 df = pd.read_csv(path)
-df = df[:10000]
+df = df[:15000]
 text = df[df['Language'] == 'en']['Text'].apply(lambda x: p.clean(x))
 labels = df[df['Language'] == 'en']['Label']
 
